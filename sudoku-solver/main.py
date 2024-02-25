699640a4b86fa3355f98ad6bc8ebfbe42d7a3b2a
Lavanya-31 <118906551+Lavanya-31@users.noreply.github.com>
2024-02-25 22:33:04 +0530

    Create main.py

---
+++ sudoku-solver/main.py
@@ -0,0 +1,70 @@
+def print_board(board):
+    for i in range(9):
+        if i % 3 == 0 and i != 0:
+            print("-" * 21)
+        for j in range(9):
+            if j % 3 == 0 and j != 0:
+                print("|", end=" ")
+            print(board[i][j], end=" ")
+        print()
+
+def find_empty_location(board):
+    for i in range(9):
+        for j in range(9):
+            if board[i][j] == 0:
+                return i, j
+    return None
+
+def is_valid(board, num, position):
+    for i in range(9):
+        if board[position[0]][i] == num or board[i][position[1]] == num:
+            return False
+
+    box_row, box_col = position[0] // 3 * 3, position[1] // 3 * 3
+    for i in range(3):
+        for j in range(3):
+            if board[box_row + i][box_col + j] == num:
+                return False
+
+    return True
+
+def solve_sudoku(board):
+    empty = find_empty_location(board)
+    if not empty:
+        return True
+
+    row, col = empty
+
+    for num in range(1, 10):
+        if is_valid(board, num, (row, col)):
+            board[row][col] = num
+
+            if solve_sudoku(board):
+                return True
+
+            board[row][col] = 0  # Backtrack if the current configuration is not valid
+
+    return False
+
+if __name__ == "__main__":
+    sudoku_board = [
+        [5, 3, 0, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        [0, 0, 0, 0, 8, 0, 0, 7, 9]
+    ]
+
+    print("Sudoku Puzzle:")
+    print_board(sudoku_board)
+    print("\nSolving...\n")
+
+    if solve_sudoku(sudoku_board):
+        print("Sudoku Solution:")
+        print_board(sudoku_board)
+    else:
+        print("No solution exists.")
